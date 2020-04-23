ba1e84def9d425f63f4a009374c8fc435542f412
tlduf95 <tlduf95@naver.com>
2020-04-23 19:38:43 +0900

    commit

--- index.js
+++ index.js
@@ -2,16 +2,38 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://tlduf95:wdk1cjf1@react-mongo-first-pyky1.mongodb.net/test?retryWrites=true&w=majority'
 , {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
-}).then(() => console.log('MongoDB 연결'))
+}).then(() => console.log('MongoDB conneted'))
     .catch(err => console.log(err))
 
 
 
 app.get('/', (req, res) => res.send('Hello World! 안녕하세요'))
+
+app.post('/register', (req, res) => {
+    //회원가입 필요 정보 client 가져옴
+    //가져온 정보 DB에 넣음
+
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({ success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
--- models/User.js
+++ models/User.js
@@ -33,4 +33,4 @@ const userSchema = mongoose.Schema({
 
 const User = mongoose.model('User', userSchema)
 
-module.export = { User }
+module.exports = { User }
