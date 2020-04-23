2b3c9c41f445f57014034f877863c551cfe04796
tlduf95 <tlduf95@naver.com>
2020-04-23 20:27:09 +0900

    commit

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://tlduf95:wdk1cjf1@react-mongo-first-pyky1.mongodb.net/test?retryWrites=true&w=majority'
+mongoose.connect('mongodb+srv://tlduf95:wkd1cjf1@react-mongo-first-pyky1.mongodb.net/test?retryWrites=true&w=majority'
 , {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
-}).then(() => console.log('MongoDB conneted'))
+}).then(() => console.log('MongoDB Conneted'))
     .catch(err => console.log(err))
 
 
@@ -29,11 +29,11 @@ app.post('/register', (req, res) => {
     const user = new User(req.body)
 
     user.save((err, userInfo) => {
-        if(err) return res.json({ success: false, err})
+        if (err) return res.json({ success: false, err})
         return res.status(200).json({
             success: true
         })
-    })
-})
+    });
+});
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
